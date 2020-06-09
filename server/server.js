3bfc6372dcf523732934bef91ac6473ccfdfbbaf
AndreaBruno97 <andbruno97@gmail.com>
2020-06-09 15:50:23 +0200

    fix: created database and first test

--- server/server.js
+++ server/server.js
@@ -5,3 +5,10 @@ const PORT = 3001;
 app = new express();
 
 app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
+
+// Db inizialization
+const sqlite = require('sqlite3');
+const db = new sqlite.Database('car_rental_database.bd3', (err) => { if (err) throw err; });
+
+// Test route
+app.get('/', (req, res) => {res.json({"Hello": "World!"});});
